c6bcf1cecfd0c47e3e04a889dca37de191ceadaf
Varun Nanda <varunnanda2520@gmail.com>
2025-09-05 12:30:59 +0200

    ne testing of multiple tasks with code and setting the streams

--- dags/execute_multiple_tasks.py
+++ dags/execute_multiple_tasks.py
@@ -20,12 +20,39 @@ with DAG(
 
     taskA = BashOperator(
         task_id = 'taskA',
-        bash_command = 'echo TASK A has executed!'
+        bash_command = '''
+            echo Task A has started!
+            for i in {1..5}
+            do
+                echo Task A printing $i
+            done
+            echo Task A has completed!
+        '''
     )
 
     taskB = BashOperator(
         task_id = 'taskB',
-        bash_command = 'echo TASK B has executed!'
+        bash_command = '''
+                echo Task B has started!
+                sleep 4
+                echo Task B has ended!'''
     )
 
-taskA.set_upstream(taskB) #task b will be executed after task a
+    taskC = BashOperator(
+        task_id = 'taskC',
+        bash_command = '''
+                echo Task C has started!
+                sleep 10
+                echo Task C has ended!'''
+    )
+
+    taskD = BashOperator(
+        task_id = 'taskD',
+        bash_command='echo task D is executed!'
+    )
+
+taskA.set_downstream(taskB) #task b will be executed after task a
+taskA.set_downstream(taskC) #task c will be executed after task a
+
+taskD.set_upstream(taskB) #task d will be executed after task b 
+taskD.set_upstream(taskC) #task d will be executed after task c

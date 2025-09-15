78c057bb71dac6067b6b081b7029052f0a46a908
Varun Nanda <varunnanda2520@gmail.com>
2025-09-15 22:23:42 +0200

    experimented with branching and variables function from airflow

--- dags/executing_branching.py
+++ dags/executing_branching.py
@@ -1,29 +1,82 @@
 from datetime import datetime, timedelta
 from airflow.utils.dates import days_ago
+import pandas as pd
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 
-from random import choice
+from airflow.models import Variable
 
 default_args = {
     'owner' : 'varun25',
 }
 
-def has_driving_license():
-    return choice([True, False])
+dataset_path='/home/varun/airflow/datasets/insurance.csv'
+output_path='/home/varun/airflow/output/{0}.csv'
 
-def branch(ti):
-    if ti.xcom_pull(task_ids='has_driving_license'):
-        return 'eligible_to_drive'
-    else:
-        return 'not_eligible_to_drive'                      
 
-def eligible_to_drive():
-    print("You can drive, you have a license!")
 
-def not_eligible_to_drive():
-    print("I'm afraid you are out of luck, you need a license to drive")
+def read_csv_file():
+    df = pd.read_csv(dataset_path)
+    print(df.head())
+    return df.to_json()  #returning dataframe as json object
+
+def remove_null_values(ti):
+    json_data=ti.xcom_pull(task_ids='read_csv_file')
+    df=pd.read_json(json_data)
+    df=df.dropna()
+    print(df)
+    return df.to_json()
+
+def determine_branch():
+    transform_action=Variable.get('transform_action', default_var=None)
+    if transform_action.startswith('filter'):
+        return transform_action
+    elif transform_action=='groupby_region_smoker':
+        return 'groupby_region_smoker'
+    
+def filter_by_southwest(ti):
+    json_data=ti.xcom_pull(task_ids='remove_null_values')
+    df=pd.read_json(json_data)
+    filtered_df=df[df['region']=='southwest']
+    filtered_df.to_csv(output_path.format('southwest'), index=False)
+
+def filter_by_southeast(ti):
+    json_data=ti.xcom_pull(task_ids='remove_null_values')
+    df=pd.read_json(json_data)
+    filtered_df=df[df['region']=='southeast']
+    filtered_df.to_csv(output_path.format('southeast'), index=False)
+
+def filter_by_northwest(ti):
+    json_data=ti.xcom_pull(task_ids='remove_null_values')
+    df=pd.read_json(json_data)
+    filtered_df=df[df['region']=='northwest']
+    filtered_df.to_csv(output_path.format('northwest'), index=False)
+
+def filter_by_northeast(ti):
+    json_data=ti.xcom_pull(task_ids='remove_null_values')
+    df=pd.read_json(json_data)
+    filtered_df=df[df['region']=='northeast']
+    filtered_df.to_csv(output_path.format('northeast'), index=False)
+
+
+def groupby_region_smoker(ti):
+    json_data=ti.xcom_pull(task_ids='remove_null_values')
+    df=pd.read_json(json_data)
+    region_smoker_df=df.groupby(['region']).agg({
+        'charges':'mean',
+        'age':'mean',
+        'bmi':'mean'
+        }).reset_index() # calculating the mean chanrges, age and bmi , grouperd by region and smoker
+    region_smoker_df.to_csv(output_path.format('grouped_by_region'), index=False)
+
+    smoker_df=df.groupby(['smoker']).agg({
+        'charges':'mean',
+        'age':'mean',
+        'bmi':'mean'
+        }).reset_index() # calculating the mean chanrges, age and bmi , grouperd by region and smoker
+    smoker_df.to_csv(output_path.format('grouped_by_smoker'), index=False)
+
 
 
 with DAG(
@@ -32,31 +85,51 @@ with DAG(
     default_args = default_args,
     start_date = days_ago(1),
     schedule_interval = '@once',
-    tags = ['branching', 'conditions']
+    tags = ['python', 'branching', 'pipeline', 'transform']
 ) as dag:
 
-    taskA = PythonOperator(
-        task_id = 'has_driving_license',
-        python_callable = has_driving_license              
+    read_csv_file_task = PythonOperator(
+        task_id = 'read_csv_file',
+        python_callable = read_csv_file             
     )
 
-    taskB = BranchPythonOperator(
-        task_id = 'branch',
-        python_callable = branch
+    remove_null_values_task = PythonOperator(
+        task_id = 'remove_null_values',
+        python_callable = remove_null_values              
     )
 
-    taskC = PythonOperator(
-        task_id = 'eligible_to_drive',
-        python_callable = eligible_to_drive              
+    determine_branch_task = BranchPythonOperator(
+        task_id = 'determine_branch',
+        python_callable = determine_branch
+    )
+    filter_by_northeast_task = PythonOperator(
+        task_id = 'filter_by_northeast',
+        python_callable = filter_by_northeast              
+    )
+    filter_by_northwest_task = PythonOperator(
+        task_id = 'filter_by_northwest',
+        python_callable = filter_by_northwest              
+    )
+    filter_by_southeast_task = PythonOperator(
+        task_id = 'filter_by_southeast',
+        python_callable = filter_by_southeast              
+    )
+    filter_by_southwest_task = PythonOperator(
+        task_id = 'filter_by_southwest',
+        python_callable = filter_by_southwest              
+    )
+    groupby_region_smoker_task = PythonOperator(
+        task_id = 'groupby_region_smoker',
+        python_callable = groupby_region_smoker              
     )
 
-    taskD = PythonOperator(
-        task_id = 'not_eligible_to_drive',
-        python_callable = not_eligible_to_drive              
-    )
+    
 
-taskA >> taskB >> [taskC, taskD]
-
+read_csv_file_task >> remove_null_values_task >> determine_branch_task>>[filter_by_northeast_task,
+                                                                         filter_by_northwest_task,
+                                                                         filter_by_southeast_task, 
+                                                                         filter_by_southwest_task, 
+                                                                         groupby_region_smoker_task]
 
 
 
